7ef41aebe389ab1b49eba8f26edad5c7f4e92f91
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2023-05-02 16:36:16 -0300

    Update JurosComposto.vue

--- src/pages/JurosComposto.vue
+++ src/pages/JurosComposto.vue
@@ -3,19 +3,19 @@
   <NavBar/>
 
   <div class="flex justify-center place-content-center pt-6 relative top-3">
-    <div class="w-5/6 h-32 p-4 text-center rounded-lg sm:p-8 bg-green-light">
-      <div class='flex flex-row gap-4 place-content-center'>
+    <div class=" p-4 text-center rounded-lg sm:p-8 bg-green-light sm:w-5/6 sm:h-32">
+      <div class='flex flex-col gap-4 place-items-center place-content-center sm:flex-row'>
 
         <div class="flex flex-col text-left">
-          <label for="Capital Inicial" class="block mb-1 text-sm font-medium text-input-text">Capital Inicial</label>
-          <input v-model="capitalInicial" type="text" class="bg-input rounded-md border-b-4 border-gray-300 focus:border-brand-green p-2 focus:outline-none" placeholder="R$ 0,00">
+          <label for="Capital Inicial" class="block mb-1 text-sm font-medium text-input-text">Aumento</label>
+          <input v-model="capitalInicial" type="text" class="w-72 md:w-64 bg-input rounded-md border-b-4 border-gray-300 focus:border-brand-green p-2 focus:outline-none" placeholder="R$ 0,00">
         </div>
         
         <div class="flex flex-col text-left">
           <label for="taxa de juros" class="block mb-1 text-sm font-medium text-input-text">Taxa de Juros</label>
           <div class="flex">
-            <input v-model="juros" type="text" class="bg-input rounded-tl-md rounded-bl-md border-b-4 border-gray-300 focus:border-brand-green p-2 focus:outline-none" placeholder="0 %">
-            <select v-model="jurosTipo" class="w-17 bg-input text-gray-400 text-sm rounded-r-md border-b-4 border-gray-300 border-l-2 block p-2">
+            <input v-model="juros" type="text" class="w-56 md:w-48 bg-input rounded-tl-md rounded-bl-md border-b-4 border-gray-300 focus:border-brand-green p-2 focus:outline-none" placeholder="0 %">
+            <select v-model="jurosTipo" class="md:w-16 bg-input text-gray-400 text-xs md:text-sm rounded-r-md border-b-4 border-gray-300 border-l-2 p-2">
                 <option value="anual">Anual</option>
                 <option value="mensal">Mensal</option>
             </select>
@@ -25,15 +25,15 @@
         <div class="flex flex-col text-left">
           <label for="Tempo" class="block mb-1 text-sm font-medium text-input-text">Tempo</label>
           <div class="flex">
-            <input v-model="tempo" type="number" class="bg-input rounded-tl-md rounded-bl-md border-b-4 border-gray-300 focus:border-brand-green p-2 focus:outline-none" placeholder="0">
-            <select v-model="tempoTipo" class="w-17 bg-input text-gray-400 text-sm rounded-r-md border-b-4 border-gray-300 border-l-2 block p-2">
+            <input v-model="tempo" type="number" class="w-56 md:w-48 bg-input rounded-tl-md rounded-bl-md border-b-4 border-gray-300 focus:border-brand-green p-2 focus:outline-none" placeholder="0">
+            <select v-model="tempoTipo" class="md:w-16 bg-input text-gray-400 text-xs md:text-sm rounded-r-md border-b-4 border-gray-300 border-l-2 p-2">
                 <option value="anual">Anual</option>
                 <option value="mensal">Mensal</option>
             </select>
           </div>
         </div>
 
-        <div class="relative top-6">
+        <div class="relative sm:top-2.5">
           <button @click="calcular" class="bg-brand-green w-20 h-10 text-white rounded-sm">Calcular</button>
         </div>
         
@@ -83,10 +83,11 @@ export default {
   },
   methods: {
     calcular() {
-      if (!this.capitalInicial ||!this.aumento || !this.taxa) {
+      if (!this.capitalInicial ||!this.juros || !this.tempo) {
         this.resultado = '';
         return;
       }
+      
       const capital = parseFloat(this.capitalInicial.replace(',', '.'));
       const juros = parseFloat(this.juros.replace(',', '.'));
       const tempo = parseInt(this.tempo);
