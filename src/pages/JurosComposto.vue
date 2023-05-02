6a6e8748797c11635d03e86462005a862414da44
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2023-05-02 17:43:57 -0300

    Update JurosComposto.vue

--- src/pages/JurosComposto.vue
+++ src/pages/JurosComposto.vue
@@ -7,7 +7,7 @@
       <div class='flex flex-col gap-4 place-items-center place-content-center sm:flex-row'>
 
         <div class="flex flex-col text-left">
-          <label for="Capital Inicial" class="block mb-1 text-sm font-medium text-input-text">Aumento</label>
+          <label for="Capital Inicial" class="block mb-1 text-sm font-medium text-input-text">Capital Inicial</label>
           <input v-model="capitalInicial" type="text" class="w-72 md:w-64 bg-input rounded-md border-b-4 border-gray-300 focus:border-brand-green p-2 focus:outline-none" placeholder="R$ 0,00">
         </div>
         
