5a16ec160ac1a980099d04db423c36850539b395
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2023-05-02 22:24:29 -0300

    Update ValorInicialComposto.vue

--- src/pages/ValorInicialComposto.vue
+++ src/pages/ValorInicialComposto.vue
@@ -44,16 +44,19 @@
   <div class="flex justify-center">
     <div class="w-10/12 h-Table fixed p-4 text-center rounded-lg sm:p-8 bg-brand-green text-white">
       <div v-if="resultado" class="text-xl font-bold mt-4">
-        <p>{{ step01 }}<sup>{{ stepTempo }}</sup></p>
-        <p>{{ step02 }}<sup>{{ stepTempo }}</sup></p>
-        <p>{{ step03 }}</p>
-        <p>{{ resultado }}</p>
+        <div v-html="part1"></div>
+        <div v-html="part2"></div>
+        <div v-html="part3"></div>
+        <div v-html="resultado"></div>
       </div>
     </div>
   </div>
 
 </template>
 <script>
+
+import katex from 'katex';
+import 'katex/dist/katex.min.css';
 
 import NavBar from '../components/NavBar.vue'
 
@@ -70,9 +73,9 @@ export default {
       tempo: '',
       tempoTipo: 'anual',
       resultado: '',
-      stepTempo: '',
-      step01: '',
-      step02: '',
+      part1: '',
+      part2: '',
+      part3: '',      
     }
   },
   methods: {
@@ -85,16 +88,15 @@ export default {
       const juros = parseFloat(this.juros.replace(',', '.'));
       const tempo = parseInt(this.tempo);
 
-      const jurosFinal = this.jurosTipo === this.tempoTipo ? juros : juros / 12;
+      const jurosFinal = this.jurosTipo === this.tempoTipo ? juros/100 : (juros / 12)/100;
 
-      const ValorInicialComposto = valorFinal / (1 + jurosFinal / 100) ** (tempo);
+      const ValorInicialComposto = valorFinal / (1 + jurosFinal) ** (tempo);
 
-      this.stepTempo = `${tempoAnual}`
-      this.step01 = `Vi = ${valorFinal} / (1 + ${jurosFinal/100})`;
-      this.step02 = `Vi = ${valorFinal} / (${1 + jurosFinal/100})`;
-      this.step03 = `Vi = ${valorFinal} / ${((1 + (jurosFinal/100)) ** tempo).toFixed(6)}`;
+      this.part1 = katex.renderToString(`Vi = ${valorFinal} / (1 + ${jurosFinal})^{${tempo}}`);
+      this.part2 = katex.renderToString(`Vi = ${valorFinal} / ${1+jurosFinal}^{${tempo}}`);
+      this.part3 = katex.renderToString(`Vi = ${valorFinal} / ${((1+jurosFinal)**tempo).toFixed(6)}`);
 
-      this.resultado = `Vi = R$ ${ValorInicialComposto.toFixed(2)}`;
+      this.resultado = katex.renderToString(`Vi = R$ ${ValorInicialComposto.toFixed(2)}`);
     }
   }
 }
