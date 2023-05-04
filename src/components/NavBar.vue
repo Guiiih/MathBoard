fe84788a5b1a1e980de421d5de6f5b794e345bae
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2023-05-04 11:13:16 -0300

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,32 +1,41 @@
-<template>
-    <nav class="bg-gray-800">
-      <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between h-16">
-          <div class="flex items-center">
-            <div class="flex-shrink-0">
-              <img class="h-8 w-8" src="/img/logo.svg" alt="Logo">
-            </div>
-            <div class="hidden md:block">
-              <div class="ml-10 flex items-baseline space-x-4">
-                <router-link to="/" class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700">Home</router-link>
-                <router-link to="/about" class="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-gray-700 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700">About</router-link>
-                <router-link to="/contact" class="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-gray-700 hover:text-white focus:outline-none focus:text-white focus:bg-gray-700">Contact</router-link>
-              </div>
-            </div>
-          </div>
-          <div class="-mr-2 flex md:hidden">
-            <button type="button" class="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white" @click="toggleMenu" :aria-expanded="menuIsOpen">
-              <span class="sr-only">Open main menu</span>
-              <svg :class="{'hidden': menuIsOpen, 'block': !menuIsOpen}" class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
-              <svg :class="{'hidden': !menuIsOpen, 'block': menuIsOpen}" class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-              </svg>
+<template lang="">   
+    <nav>
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <router-link to="/" class="flex items-center">
+                <img src="../assets/images/MathBoard.svg" class="h-6 mr-3" alt="Math Logo" />
+            </router-link>
+        <div class="relative inline-block text-left">
+
+            <button class="w-44 text-brand-green border font-medium rounded-lg text-sm px-4 py-2.5 flex justify-center items-center " type="button" @click.stop.prevent="toggle()">
+                Operações
+                <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
             </button>
-          </div>
+
+            <div class="absolute z-10 my-2 bg-white divide-y divide-gray-100 rounded-lg shadow w-52 " v-show="isOpen">
+                <ul class="py-2 text-sm  text-brand-green">
+                    <li>
+                        <router-link to="/" class="block px-4 py-2 hover:text-green-700">Juros Simples</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/JurosComposto" class="block px-4 py-2 hover:text-green-700">Juros Composto</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/ValorInicialSimples" class="block px-4 py-2 hover:text-green-700">Valor Inicial Simples</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/ValorInicialComposto" class="block px-4 py-2 hover:text-green-700">Valor Inicial Composto</router-link>
+                    </li>
+                    
+                    <li>
+                        <router-link to="/AumentoCapitalSimples" class="block px-4 py-2 hover:text-green-700">Aumento Capital Simples</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/AumentoCapitalComposto" class="block px-4 py-2 hover:text-green-700">Aumento Capital Composto</router-link>
+                    </li>
+                </ul>
+            </div>
+            </div>
         </div>
-      </div>
     </nav>
 </template>
 
