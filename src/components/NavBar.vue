e6de403809ab1e84cbdde9e990f299d87c19445f
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2025-06-17 11:31:10 -0300

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -20,22 +20,14 @@
 
           <div class="hidden sm:block sm:ml-6">
             <div class="relative inline-block text-left">
-                <button class="text-brand-green border font-medium rounded-lg text-sm px-4 py-2.5 md:flex flex justify-center items-center  " type="button" @click.stop.prevent="toggleMenu()">
+                <button class="text-brand-green border font-medium rounded-lg text-sm px-4 py-2.5 md:flex flex justify-center items-center" type="button" @click.stop.prevent="toggleMenu()">
                     Operações
                     <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                     </svg>
                 </button>
-                <div class="absolute z-10 my-2 bg-white divide-y divide-gray-100 rounded-lg shadow w-52 py-2 text-sm text-brand-green" v-show="isMenuOpen"> 
-                    <router-link to="/" class="block px-4 py-2 hover:text-green-700">Juros Simples</router-link>
-                    <router-link to="/JurosComposto" class="block px-4 py-2 hover:text-green-700">Juros Composto</router-link>
-                    <router-link to="/ValorInicialSimples" class="block px-4 py-2 hover:text-green-700">Valor Inicial Simples</router-link>
-                    <router-link to="/ValorInicialComposto" class="block px-4 py-2 hover:text-green-700">Valor Inicial Composto</router-link>                  
-                    <router-link to="/AumentoCapitalSimplesV1" class="block px-4 py-2 hover:text-green-700">Aumento Capital Simples V1</router-link>
-                    <router-link to="/AumentoCapitalSimplesV2" class="block px-4 py-2 hover:text-green-700">Aumento Capital Simples V2</router-link>
-                    <router-link to="/AumentoCapitalCompostoV1" class="block px-4 py-2 hover:text-green-700">Aumento Capital Composto V1</router-link>
-                    <router-link to="/AumentoCapitalCompostoV2" class="block px-4 py-2 hover:text-green-700">Aumento Capital Composto V2</router-link>
-                    <router-link to="/SAF" class="block px-4 py-2 hover:text-green-700">SAF</router-link>
+                <div class="absolute z-10 my-2 bg-white divide-y divide-gray-100 rounded-lg shadow w-56 py-2 text-sm text-brand-green overflow-y-auto max-h-96" v-show="isMenuOpen">
+                    <router-link v-for="link in links" :key="link.to" :to="link.to" @click="isMenuOpen = false" class="block px-4 py-2 hover:text-green-700">{{ link.text }}</router-link>
                 </div>
             </div>
           </div>
@@ -43,18 +35,8 @@
       </div>
 
       <div :class="{'block ': isMenuOpen, 'hidden': !isMenuOpen}" class="sm:hidden">
-        <div class="py-2 text-sm text-brand-green">
-            <router-link to="/" class="block px-4 py-2 hover:text-green-700">Juros Simples</router-link>
-            <router-link to="/JurosComposto" class="block px-4 py-2 hover:text-green-700">Juros Composto</router-link>
-            <router-link to="/ValorInicialSimples" class="block px-4 py-2 hover:text-green-700">Valor Inicial Simples</router-link>
-            <router-link to="/ValorInicialComposto" class="block px-4 py-2 hover:text-green-700">Valor Inicial Composto</router-link>                  
-            <router-link to="/AumentoCapitalSimples" class="block px-4 py-2 hover:text-green-700">Aumento Capital Simples</router-link>
-            <router-link to="/AumentoCapitalComposto" class="block px-4 py-2 hover:text-green-700">Aumento Capital Composto</router-link>
-            <router-link to="/AumentoCapitalSimplesV1" class="block px-4 py-2 hover:text-green-700">Aumento Capital Simples V1</router-link>
-            <router-link to="/AumentoCapitalSimplesV2" class="block px-4 py-2 hover:text-green-700">Aumento Capital Simples V2</router-link>
-            <router-link to="/AumentoCapitalCompostoV1" class="block px-4 py-2 hover:text-green-700">Aumento Capital Composto V1</router-link>
-            <router-link to="/AumentoCapitalCompostoV2" class="block px-4 py-2 hover:text-green-700">Aumento Capital Composto V2</router-link>
-            <router-link to="/SAF" class="block px-4 py-2 hover:text-green-700">SAF</router-link>
+        <div class="py-2 text-sm text-brand-green overflow-y-auto max-h-96">
+            <router-link v-for="link in links" :key="link.to" :to="link.to" @click="isMenuOpen = false" class="block px-4 py-2 hover:text-green-700">{{ link.text }}</router-link>
         </div>
     </div>
     </nav>
@@ -63,7 +45,31 @@
 export default {
   data() {
     return {
-        isMenuOpen: false
+        isMenuOpen: false,
+        links: [
+            { to: '/', text: 'Juros Simples' },
+            { to: '/JurosComposto', text: 'Juros Composto' },
+            { to: '/ValorInicialSimples', text: 'Valor Inicial Simples' },
+            { to: '/ValorInicialComposto', text: 'Valor Inicial Composto' },
+            { to: '/AumentoCapitalSimplesV1', text: 'Aumento Capital Simples V1' },
+            { to: '/AumentoCapitalSimplesV2', text: 'Aumento Capital Simples V2' },
+            { to: '/AumentoCapitalCompostoV1', text: 'Aumento Capital Composto V1' },
+            { to: '/AumentoCapitalCompostoV2', text: 'Aumento Capital Composto V2' },
+            { to: '/SAF', text: 'SAF' },
+            { to: '/SAC', text: 'SAC' },
+            { to: '/SAM', text: 'SAM' },
+            { to: '/SSA', text: 'SSA' },
+            { to: '/CorrecaoMonetaria01', text: 'Correção Monetária 01' },
+            { to: '/CorrecaoMonetaria02', text: 'Correção Monetária 02' },
+            { to: '/DescontoCompostoComercial', text: 'Desconto Composto Comercial' },
+            { to: '/DescontoCompostoRacional', text: 'Desconto Composto Racional' },
+            { to: '/DescontoSimplesComercial', text: 'Desconto Simples Comercial' },
+            { to: '/DescontoSimplesRacional', text: 'Desconto Simples Racional' },
+            { to: '/PagamentoAntecipado', text: 'Pagamento Antecipado' },
+            { to: '/SerieDiferida', text: 'Série Diferida' },
+            { to: '/SeriePostecipada', text: 'Série Postecipada' },
+            { to: '/TaxaDesvalorizacaoMoeda', text: 'Taxa Desvalorização da Moeda' },
+        ]
     }
   },
   methods: {
