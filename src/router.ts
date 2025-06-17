f5df41bd1b314763b3d7cc5497445338ce3b0287
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2025-06-17 11:26:07 -0300

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -8,9 +8,19 @@ import AumentoCapitalSimplesV2 from './pages/AumentoCapitalSimplesV2.vue'
 import AumentoCapitalCompostoV1 from './pages/AumentoCapitalCompostoV1.vue'
 import AumentoCapitalCompostoV2 from './pages/AumentoCapitalCompostoV2.vue'
 import SAF from './pages/SAF.vue'
-
-
-
+import SAC from './pages/SAC.vue'
+import SAM from './pages/SAM.vue'
+import SSA from './pages/SSA.vue'
+import CorrecaoMonetaria01 from './pages/CorreçãoMonetaria01.vue'
+import CorrecaoMonetaria02 from './pages/CorreçãoMonetaria02.vue'
+import DescontoCompostoComercial from './pages/DescontoCompostoComercial.vue'
+import DescontoCompostoRacional from './pages/DescontoCompostoRacional.vue'
+import DescontoSimplesComercial from './pages/DescontoSimplesComercial.vue'
+import DescontoSimplesRacional from './pages/DescontoSimplesRacional.vue'
+import PagamentoAntecipado from './pages/PagamentoAntecipado.vue'
+import SerieDiferida from './pages/SerieDiferida.vue'
+import SeriePostecipada from './pages/SeriePostecipada.vue'
+import TaxaDesvalorizacaoMoeda from './pages/TaxaDesvalorizaçãoMoeda.vue'
 
 const routes = [
   {
@@ -58,10 +68,75 @@ const routes = [
     name: 'SAF',
     component: SAF
   },
+  {
+    path: '/SAC',
+    name: 'SAC',
+    component: SAC
+  },
+  {
+    path: '/SAM',
+    name: 'SAM',
+    component: SAM
+  },
+  {
+    path: '/SSA',
+    name: 'SSA',
+    component: SSA
+  },
+  {
+    path: '/CorrecaoMonetaria01',
+    name: 'CorrecaoMonetaria01',
+    component: CorrecaoMonetaria01
+  },
+  {
+    path: '/CorrecaoMonetaria02',
+    name: 'CorrecaoMonetaria02',
+    component: CorrecaoMonetaria02
+  },
+  {
+    path: '/DescontoCompostoComercial',
+    name: 'DescontoCompostoComercial',
+    component: DescontoCompostoComercial
+  },
+  {
+    path: '/DescontoCompostoRacional',
+    name: 'DescontoCompostoRacional',
+    component: DescontoCompostoRacional
+  },
+  {
+    path: '/DescontoSimplesComercial',
+    name: 'DescontoSimplesComercial',
+    component: DescontoSimplesComercial
+  },
+  {
+    path: '/DescontoSimplesRacional',
+    name: 'DescontoSimplesRacional',
+    component: DescontoSimplesRacional
+  },
+  {
+    path: '/PagamentoAntecipado',
+    name: 'PagamentoAntecipado',
+    component: PagamentoAntecipado
+  },
+  {
+    path: '/SerieDiferida',
+    name: 'SerieDiferida',
+    component: SerieDiferida
+  },
+  {
+    path: '/SeriePostecipada',
+    name: 'SeriePostecipada',
+    component: SeriePostecipada
+  },
+  {
+    path: '/TaxaDesvalorizacaoMoeda',
+    name: 'TaxaDesvalorizacaoMoeda',
+    component: TaxaDesvalorizacaoMoeda
+  },
 ]
 
 const router = createRouter({
-  history: createWebHistory(), 
+  history: createWebHistory(),
   routes
 })
 
