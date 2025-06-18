daa5df4a04411bb73da2aedc8c6a39bbaee9b2e0
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2025-06-18 18:46:04 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: `/MathBoard/`, // Altere aqui!
+  base: `/MathBoard/`, 
   plugins: [vue()],
 })
