9e6ca807154c175d0f74c10d1249f20c0e306860
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2025-06-18 15:19:23 -0300

    ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: `/`, 
+  base: `/`,
   plugins: [vue()],
 })
