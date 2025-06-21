621b3afba7b79fbed718f0896a4117e16175bf22
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2025-06-21 16:29:00 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <router-view />
     <footer class="app-footer">
       <p>&copy; {{ currentYear }} {{ $t('footer.copyright') }}</p>
-      <LanguageSwitcher class="language-switcher-fixed-bottom-right" />
+      <LanguageSwitcher />
     </footer>
   </div>
 </template>
@@ -11,15 +11,11 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { provideTheme } from './composables/useTheme';
-import { useI18n } from 'vue-i18n';
 import LanguageSwitcher from './components/LanguageSwitcher.vue'; 
 
 const currentYear = ref(new Date().getFullYear());
-const { t } = useI18n();
-
 provideTheme();
 </script>
 
 <style>
-
 </style>
