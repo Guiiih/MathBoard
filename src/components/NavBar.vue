b4920198dfdd8051dc568aee355e7dc84b9eb13f
Guilherme Nascimento <guilhermenascimentopinho244@gmail.com>
2025-06-21 16:24:58 -0300

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,7 +16,7 @@
                     aria-haspopup="true"
                     aria-controls="dropdown-menu"
                 >
-                    <span class="truncate">{{ currentPageName }}</span>
+                    <span class="truncate">{{ $t(currentPageName) }}</span>
                     <svg class="nav-dropdown-icon" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                     </svg>
@@ -29,10 +29,10 @@
                         @click="isMenuOpen = false"
                         class="nav-dropdown-item"
                         role="menuitem"
-                    >{{ link.text }}</router-link>
+                    >{{ $t(link.i18nKey) }}</router-link>
                 </div>
             </div>
-            <button @click="themeContext.toggleTheme()" class="theme-toggle-button" :aria-label="themeContext.theme.value === 'light' ? 'Mudar para tema escuro' : 'Mudar para tema claro'">
+            <button @click="themeContext.toggleTheme()" class="theme-toggle-button" :aria-label="themeContext.theme.value === 'light' ? $t('navbar.darkMode') : $t('navbar.lightMode')">
               <svg v-if="themeContext.theme.value === 'light'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                 <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.592 1.591a.75.75 0 0 0 1.06 1.061l1.591-1.591ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM18.894 17.834a.75.75 0 0 0 1.06-1.06l-1.591-1.592a.75.75 0 0 0-1.061 1.06l1.592 1.591ZM12 21.75a.75.75 0 0 1-.75-.75v-2.25a.75.75 0 0 1 1.5 0v2.25a.75.75 0 0 1-.75.75ZM5.05 17.834a.75.75 0 0 0 1.06 1.06l1.591-1.592a.75.75 0 0 0-1.06-1.061l-1.592 1.591ZM3 12a.75.75 0 0 1 .75-.75h2.25a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12ZM5.05 6.166a.75.75 0 0 0-1.061 1.06l1.592 1.591a.75.75 0 0 0 1.06-1.061L5.05 6.166Z" />
               </svg>
@@ -50,47 +50,52 @@
 </template>
 
 <script>
-import { useTheme } from '../composables/useTheme'; 
+import { useTheme } from '../composables/useTheme';
+import { useI18n } from 'vue-i18n'; 
+import { computed, watch } from 'vue'; 
+import { useRoute } from 'vue-router'; 
 
 export default {
   setup() {
-    const themeContext = useTheme(); 
-    return { themeContext };
+    const themeContext = useTheme();
+    const { t } = useI18n(); 
+    const route = useRoute(); 
+
+    const links = computed(() => [
+      { to: '/', i18nKey: 'navbar.jurosSimples' },
+      { to: '/JurosComposto', i18nKey: 'navbar.jurosComposto' },
+      { to: '/ValorInicialSimples', i18nKey: 'navbar.valorInicialSimples' },
+      { to: '/ValorInicialComposto', i18nKey: 'navbar.valorInicialComposto' },
+      { to: '/AumentoCapitalSimplesV1', i18nKey: 'navbar.aumentoCapitalSimplesV1' },
+      { to: '/AumentoCapitalSimplesV2', i18nKey: 'navbar.aumentoCapitalSimplesV2' },
+      { to: '/AumentoCapitalCompostoV1', i18nKey: 'navbar.aumentoCapitalCompostoV1' },
+      { to: '/AumentoCapitalCompostoV2', i18nKey: 'navbar.aumentoCapitalCompostoV2' },
+      { to: '/SAF', i18nKey: 'navbar.saf' },
+      { to: '/SAC', i18nKey: 'navbar.sac' },
+      { to: '/SAM', i18nKey: 'navbar.sam' },
+      { to: '/SSA', i18nKey: 'navbar.ssa' },
+      { to: '/CorrecaoMonetaria01', i18nKey: 'navbar.correcaoMonetaria01' },
+      { to: '/CorrecaoMonetaria02', i18nKey: 'navbar.correcaoMonetaria02' },
+      { to: '/DescontoCompostoComercial', i18nKey: 'navbar.descontoCompostoComercial' },
+      { to: '/DescontoCompostoRacional', i18nKey: 'navbar.descontoCompostoRacional' },
+      { to: '/DescontoSimplesComercial', i18nKey: 'navbar.descontoSimplesComercial' },
+      { to: '/DescontoSimplesRacional', i18nKey: 'navbar.descontoSimplesRacional' },
+      { to: '/PagamentoAntecipado', i18nKey: 'navbar.pagamentoAntecipado' },
+      { to: '/SerieDiferida', i18nKey: 'navbar.serieDiferida' },
+      { to: '/SeriePostecipada', i18nKey: 'navbar.seriePostecipada' },
+      { to: '/TaxaDesvalorizacaoMoeda', i18nKey: 'navbar.taxaDesvalorizacaoMoeda' },
+    ]);
+
+    const currentPageName = computed(() => {
+      const currentRoute = links.value.find(link => link.to === route.path);
+      return currentRoute ? currentRoute.i18nKey : 'navbar.operations'; 
+    });
+
+    return { themeContext, links, currentPageName, t }; 
   },
   data() {
     return {
         isMenuOpen: false,
-        links: [
-            { to: '/', text: 'Juros Simples' },
-            { to: '/JurosComposto', text: 'Juros Composto' },
-            { to: '/ValorInicialSimples', text: 'Valor Inicial Simples' },
-            { to: '/ValorInicialComposto', text: 'Valor Inicial Composto' },
-            { to: '/AumentoCapitalSimplesV1', text: 'Aumento Capital Simples V1' },
-            { to: '/AumentoCapitalSimplesV2', text: 'Aumento Capital Simples V2' },
-            { to: '/AumentoCapitalCompostoV1', text: 'Aumento Capital Composto V1' },
-            { to: '/AumentoCapitalCompostoV2', text: 'Aumento Capital Composto V2' },
-            { to: '/SAF', text: 'SAF' },
-            { to: '/SAC', text: 'SAC' },
-            { to: '/SAM', text: 'SAM' },
-            { to: '/SSA', text: 'SSA' },
-            { to: '/CorrecaoMonetaria01', text: 'Correção Monetária 01' },
-            { to: '/CorrecaoMonetaria02', text: 'Correção Monetária 02' },
-            { to: '/DescontoCompostoComercial', text: 'Desconto Composto Comercial' },
-            { to: '/DescontoCompostoRacional', text: 'Desconto Composto Racional' },
-            { to: '/DescontoSimplesComercial', text: 'Desconto Simples Comercial' },
-            { to: '/DescontoSimplesRacional', text: 'Desconto Simples Racional' },
-            { to: '/PagamentoAntecipado', text: 'Pagamento Antecipado' },
-            { to: '/SerieDiferida', text: 'Série Diferida' },
-            { to: '/SeriePostecipada', text: 'Série Postecipada' },
-            { to: '/TaxaDesvalorizacaoMoeda', text: 'Taxa Desvalorização Moeda' },
-        ]
-    }
-  },
-  computed: {
-    currentPageName() {
-      const currentPath = this.$route.path;
-      const currentRoute = this.links.find(link => link.to === currentPath);
-      return currentRoute ? currentRoute.text : 'Operações';
     }
   },
   methods: {
